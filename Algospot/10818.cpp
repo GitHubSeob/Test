04c4607d89043d8479d70afffb73899d245b21cf
GitHubSeob <mnrz100@naver.com>
2021-08-05 04:23:21 +0900

    541546

---
+++ Algospot/10818.cpp
@@ -0,0 +1,24 @@
+/*
+테스트
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N = 0;
+	int num = 0;
+	int big = -1000000;
+	int small = 1000000;
+	cin >> N;
+
+	for (int x = 1; x <= N; ++x) {
+		cin >> num;
+		if (small > num) small = num;
+		if (big < num)big = num;
+	}
+	cout << small << " " << big;
+}
