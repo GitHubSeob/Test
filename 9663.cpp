213e7750864908e15b44d852c20569b00cc0b64d
GitHubSeob <githubseob@gmail.com>
2023-06-06 01:09:43 +0900

    add BOJ_9663

---
+++ 9663.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int N, answer;
+vector<int>board;
+
+void DFS(int y) {
+	int idx(0), prev(0);
+	if (y >= N) {
+		answer++;
+		return;
+	}
+	for (idx = 0; idx < N; ++idx) {
+		board[y] = idx;
+		for (prev = 0; prev < y; ++prev) {
+			if (board[prev] == board[y]) {
+				board[y] = -1;
+				break;
+			}
+			if ((y - prev) == board[y] - board[prev]) {
+				board[y] = -1;
+				break;
+			}
+			if ((y + board[y]) == (prev + board[prev])) {
+				board[y] = -1;
+					break;
+			}
+		}
+		if (board[y] != -1) {
+			DFS(y + 1);
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> N;
+
+	board = vector<int>(N, -1);
+
+	DFS(0);
+
+	cout << answer;
+}
